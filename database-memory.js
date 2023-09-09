5db4fdd9e8798a900cefbb161b5c861f53125415
giufiorenzano <giufiorenzano@gmail.com>
2023-09-08 22:55:51 -0300

    Criada a rota POST

---
+++ database-memory.js
@@ -0,0 +1,27 @@
+import { randomUUID } from "node:crypto";
+
+export class DataBaseMemory {
+  #videos = new Map();
+
+  //Set, Map
+  //Set é como um array mas não aceita valores duplicados
+  //Map é como se fosse um objeto mas com uns outros métodos
+
+  list() {
+    return this.#videos.values();
+  }
+
+  create(video) {
+    const videoId = randomUUID();
+
+    this.#videos.set(videoId, video);
+  }
+
+  update(id, video) {
+    this.#videos.set(id, video);
+  }
+
+  delete(id) {
+    this.#videos.delete(id);
+  }
+}
--- server.js
+++ server.js
@@ -10,13 +10,36 @@ server.listen(3333) */
 
 //com FASTIFY:
 import { fastify } from "fastify";
+import { DataBaseMemory } from "./database-memory.js";
 
-const server = fastify()
+const server = fastify();
 
-server.get('/', () => {
-    return 'Hello World'
-})
+const database = new DataBaseMemory();
+
+server.get("/", () => {
+  return "Hello World";
+});
+
+server.post("/videos", (req, reply) => {
+  const { title, description, duration } = req.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.put("/videos/:id", () => {
+  return "Hello World";
+});
+
+server.delete("/videos/:id", () => {
+  return "Hello World";
+});
 
 server.listen({
-    port: 3333
-})
+  port: 3333,
+});
